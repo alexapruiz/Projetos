9ad3e9f5fa159f699b91ef7a03c660be8294cb2f
Alex Ruiz <alexapruiz@hotmail.com>
2021-01-04 19:28:27 -0300

    Ajustes

--- Python/Machine_Learning/ML2.py
+++ Python/Machine_Learning/ML2.py
@@ -1,23 +1,20 @@
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
-# Informações
-# Fuma
-# Bebe
-# Obeso
+# Informações - pessoa = [# Fuma , # Bebe , # Obeso , #cardiaco]
 
-pessoa1 = [1,1,1,1]
-pessoa2 = [1,1,1,0]
-pessoa3 = [1,1,0,0]
-pessoa4 = [1,0,0,0]
-pessoa5 = [0,0,0,1]
-pessoa6 = [0,0,1,1]
-pessoa7 = [0,1,1,1]
-pessoa8 = [1,1,1,1]
-pessoa9 = [0,0,0,0]
+pessoa1 = [1,1,1]
+pessoa2 = [1,1,1]
+pessoa3 = [1,1,0]
+pessoa4 = [1,0,0]
+pessoa5 = [0,0,0]
+pessoa6 = [0,0,1]
+pessoa7 = [0,1,1]
+pessoa8 = [1,1,1]
+pessoa9 = [0,0,0]
 
 treinox = [pessoa1,pessoa2,pessoa3,pessoa4,pessoa5,pessoa6,pessoa7,pessoa8,pessoa9]
-treinoy = [1,0,0,0,0,0,0,1,0] #1 - tem doenca coração, 0 - não tem doença coração
+treinoy = [1,1,1,0,0,0,1,1,0] #1 - tem doenca coração, 0 - não tem doença coração
 
 modelo = LinearSVC()
 modelo.fit(treinox,treinoy)
--- Python/Machine_Learning/cardio.py
+++ Python/Machine_Learning/cardio.py
@@ -4,17 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 colunas = ['id','idade','genero','altura','peso','pressao_max','pressao_min','colesterol','diabetes','fuma','bebe','ativo','coracao']
-cardio = pd.read_csv('c:\\Projetos\\_Arquivos\\cardio.csv', sep=';',names=colunas,header=1)
+cardio = pd.read_csv('c:\\Projetos\\_Arquivos\\cardio2.csv', sep=';',names=colunas,header=1)
 x = cardio[['idade','peso','pressao_max','colesterol','diabetes','fuma']]
 y = cardio["coracao"]
 
-treino_x, teste_x, treino_y, teste_y = train_test_split(x,y)
+treino_x, teste_x, treino_y, teste_y = train_test_split(x,y,test_size=0.1,random_state=42)
 
 modelo = LinearSVC()
 modelo.fit(treino_x,treino_y)
 previsoes = modelo.predict(teste_x)
-print('Resultado real:')
-print(teste_y)
 print(previsoes)
-
 print(accuracy_score(teste_y,previsoes) * 100)
