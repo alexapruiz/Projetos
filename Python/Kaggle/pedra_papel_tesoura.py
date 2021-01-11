4f3c01c1f2bfb3c1b03c8aaf780e16252876574c
Alex Ruiz <alexapruiz@hotmail.com>
2021-01-11 15:57:06 -0300

    remoção da pasta kaggle

--- Python/Kaggle/pedra_papel_tesoura.py
+++
@@ -1,6 +0,0 @@
-from kaggle_environments import make
-env = make("rps", configuration={"episodeSteps": 1000}, debug = True)
-
-env.reset()
-env.run(["beat_a_human.py", "beat_a_human.py"])
-env.render(mode="ipython", width=550, height=500)
