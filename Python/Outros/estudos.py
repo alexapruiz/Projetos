4d9a7bcdac93da0b97e0b2ce9ae9e08fdf93d6da
Alex Ruiz <alexapruiz@hotmail.com>
2021-01-22 19:01:51 -0300

    Testes

--- Python/Outros/estudos.py
+++ Python/Outros/estudos.py
@@ -1,27 +1,28 @@
-#Definindo a lista de cores
-cores = {'limpa':'\033[m',
-         'azul':'\033[34m',
-         'amarelo':'\033[33m',
-         'pretoebranco':'\033[7;30m',}
+def Informa_Maior_Menor(a,b,c):
+    #Definindo a lista de cores
+    cores = {'limpa':'\033[m',
+             'azul':'\033[34m',
+             'amarelo':'\033[33m',
+             'pretoebranco':'\033[7;30m',}
 
-a = input("Informe um número")
-b = input("Informe um número")
-c = input("Informe um número")
+    a = input("Informe um número")
+    b = input("Informe um número")
+    c = input("Informe um número")
 
-#Verificando o menor
-menor = a
-if (b < a) and (b < c):
-    menor = b
-if (c < a) and (c < b):
-    menor = c
+    #Verificando o menor
+    menor = a
+    if (b < a) and (b < c):
+        menor = b
+    if (c < a) and (c < b):
+        menor = c
 
-#Verificando o maior
-maior = a
-if (b > a) and (b > c):
-    maior = b
-if (c > a) and (c > b):
-    maior = c
+    #Verificando o maior
+    maior = a
+    if (b > a) and (b > c):
+        maior = b
+    if (c > a) and (c > b):
+        maior = c
 
-print('O menor valor informado foi: {} '.format(cores['azul']),menor)
+    print('O menor valor informado foi: {} '.format(cores['azul']),menor)
 
-print('O MAIOR valor informado foi: {} '.format(cores['amarelo']),maior)
+    print('O MAIOR valor informado foi: {} '.format(cores['amarelo']),maior)
