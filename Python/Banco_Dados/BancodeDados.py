f8cc066f1196e605a479b6c91956e7d44384b962
Alex Ruiz <alexapruiz@hotmail.com>
2021-02-04 19:18:29 -0300

    Acertos biblioteca de banco de dados

--- Python/Banco_Dados/BancodeDados.py
+++ Python/Banco_Dados/BancodeDados.py
@@ -17,9 +17,20 @@ class SQLServer():
         return "Comando Executado com Sucesso!!!"
 
 class Banco_SQLite():
+
     def ConsultaSQL(arquivo, ComandoSQL) -> object:
         import sqlite3
 
         conexao = sqlite3.connect(arquivo)
         cursor = conexao.cursor()
+        cursor.execute(ComandoSQL)
         return cursor
+
+    def ExecutaComandoSQL(arquivo, ComandoSQL) -> object:
+        import sqlite3
+
+        conexao = sqlite3.connect(arquivo)
+        conexao.execute(ComandoSQL)
+        conexao.commit()
+        conexao.close()
+        return True
--- Python/Banco_Dados/SQL2.py
+++ Python/Banco_Dados/SQL2.py
@@ -3,8 +3,12 @@ import sqlite3
 sys.path.append('c:\\Projetos\\Python\\Banco_Dados')
 from BancodeDados import Banco_SQLite
 
-cursor_clientes = Banco_SQLite.ConsultaSQL('C:\\Projetos\\Python\\Banco_Dados\\Banco1.db','select * from Clientes')
-#clientes = cursor_clientes.fetchall()
-cursor_clientes.fetchall()
+if not (Banco_SQLite.ExecutaComandoSQL('Banco1.db',"update Clientes set Nome='Vitor' where Codigo = 1")):
+    print('Erro')
+
+if not (Banco_SQLite.ExecutaComandoSQL('Banco1.db',"update Clientes set Nome='Alex' where Codigo = 2")):
+    print('Erro')
+
+cursor_clientes = Banco_SQLite.ConsultaSQL('Banco1.db',"select * from Clientes")
 for rec in cursor_clientes:
     print(rec)
