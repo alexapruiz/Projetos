17628c922610c366a9bd512e43051121c09cb7be
Alex Ruiz <alexapruiz@hotmail.com>
2021-02-27 14:17:02 -0300

    Reorganizando os fontes

---
+++ Python/Arquivos/teste.py
@@ -0,0 +1,8 @@
+def NextRole():
+    return {'posicao' : 'Sr. Data Engineer',
+            'salario_min' : 70.000,
+            'salario_max': 120.000,
+            'local': 'remoto'
+            }
+
+print(NextRole())
